5db81fb873330c51b52e911469381a1f7d3f33a2
Dmytro Chyzhykov (ffbit) <dmytro.chyzhykov@yandex.ru>
2013-06-11 23:21:58 +0300

    Switched to Maven annotations

--- src/main/java/com/ffbit/maven/solr/RunSolrMojo.java
+++ src/main/java/com/ffbit/maven/solr/RunSolrMojo.java
@@ -3,13 +3,12 @@ package com.ffbit.maven.solr;
 import com.ffbit.maven.solr.jetty.JettyRunner;
 import org.apache.maven.plugin.MojoExecutionException;
 import org.apache.maven.plugin.MojoFailureException;
+import org.apache.maven.plugins.annotations.Mojo;
 
 /**
  * Goal which runs a new Apache Solr instance and executes it indefinably.
- *
- * @goal run
- * @phase pre-integration-test
  */
+@Mojo(name = "run")
 public class RunSolrMojo extends AbstractSolrMojo {
 
     @Override
--- src/main/java/com/ffbit/maven/solr/StartSolrMojo.java
+++ src/main/java/com/ffbit/maven/solr/StartSolrMojo.java
@@ -4,13 +4,12 @@ package com.ffbit.maven.solr;
 import com.ffbit.maven.solr.jetty.JettyRunner;
 import org.apache.maven.plugin.MojoExecutionException;
 import org.apache.maven.plugin.MojoFailureException;
+import org.apache.maven.plugins.annotations.Mojo;
 
 /**
  * Goal which starts a new Apache Solr instance and executes while Maven is running.
- *
- * @goal start
- * @phase pre-integration-test
  */
+@Mojo(name = "start")
 public class StartSolrMojo extends AbstractSolrMojo {
 
     @Override
