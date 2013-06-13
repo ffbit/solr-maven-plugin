0bc45b058ffb8ebd604524abfd48ed70da455760
Dmytro Chyzhykov (ffbit) <dmytro.chyzhykov@yandex.ru>
2013-06-13 11:39:05 +0300

    Mock test for HelpMojo

--- src/test/java/com/ffbit/maven/solr/HelpMojoTest.java
+++ src/test/java/com/ffbit/maven/solr/HelpMojoTest.java
@@ -1,8 +1,13 @@
 package com.ffbit.maven.solr;
 
 import org.apache.maven.plugin.Mojo;
+import org.apache.maven.plugin.logging.Log;
 import org.apache.maven.plugin.testing.AbstractMojoTestCase;
 import org.apache.maven.project.MavenProject;
+
+import static org.mockito.Mockito.atLeastOnce;
+import static org.mockito.Mockito.spy;
+import static org.mockito.Mockito.verify;
 
 public class HelpMojoTest extends AbstractMojoTestCase {
     private MavenProject project;
@@ -17,7 +22,12 @@ public class HelpMojoTest extends AbstractMojoTestCase {
     public void testHelpMessages() throws Exception {
         Mojo mojo = lookupConfiguredMojo(project, "help");
 
+        Log spiedLog = spy(mojo.getLog());
+        mojo.setLog(spiedLog);
+
         mojo.execute();
+
+        verify(spiedLog, atLeastOnce());
     }
 
 }
