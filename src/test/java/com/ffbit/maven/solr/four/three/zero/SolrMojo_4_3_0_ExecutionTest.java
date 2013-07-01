583b04e2cfdea21242e20846092096bc7eb49934
Dmytro Chyzhykov (ffbit) <dmytro.chyzhykov@yandex.ru>
2013-07-02 00:44:22 +0300

    Test configuration for Apache Solr, version 4.3.0

---
+++ src/test/java/com/ffbit/maven/solr/four/three/zero/SolrMojo_4_3_0_ExecutionTest.java
@@ -0,0 +1,15 @@
+package com.ffbit.maven.solr.four.three.zero;
+
+import com.ffbit.maven.solr.AbstractSolrMojoExecutionTest;
+
+import java.io.File;
+
+public class SolrMojo_4_3_0_ExecutionTest extends
+        AbstractSolrMojoExecutionTest {
+
+    @Override
+    public File getPom() {
+        return new File(getBasedir(), "src/test/resources/poms/4.3.0/pom.xml");
+    }
+
+}
