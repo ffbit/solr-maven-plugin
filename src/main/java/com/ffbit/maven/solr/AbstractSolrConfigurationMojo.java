a64ff83d5678719eba928d7897a46671a84243d1
Dmytro Chyzhykov (ffbit) <dmytro.chyzhykov@yandex.ru>
2013-07-03 00:58:34 +0300

    Introduced general place for Apache Solr Maven plugin configuration stuff

---
+++ src/main/java/com/ffbit/maven/solr/AbstractSolrConfigurationMojo.java
@@ -0,0 +1,77 @@
+package com.ffbit.maven.solr;
+
+import org.apache.maven.plugin.AbstractMojo;
+import org.apache.maven.plugins.annotations.Component;
+import org.apache.maven.plugins.annotations.Parameter;
+import org.sonatype.aether.RepositorySystem;
+import org.sonatype.aether.RepositorySystemSession;
+import org.sonatype.aether.repository.RemoteRepository;
+
+import java.io.File;
+import java.util.List;
+
+/**
+ * General place for Solr Maven Plugin configuration stuff.
+ */
+public abstract class AbstractSolrConfigurationMojo extends AbstractMojo {
+
+    /**
+     * The context path for the server instance.
+     *
+     * @since 0.0.1
+     */
+    @Parameter(property = "contextPath", defaultValue = "/")
+    protected String contextPath;
+
+    /**
+     * The running port.
+     *
+     * @since 0.0.1
+     */
+    @Parameter(property = "port", defaultValue = "8983")
+    protected int port;
+
+    /**
+     * The running Apache Solr version.
+     *
+     * @since 0.0.1
+     */
+    @Parameter(property = "solrVersion", defaultValue = "4.3.0")
+    protected String solrVersion;
+
+    /**
+     * The Apache Solr Home directory.
+     *
+     * @since 0.0.1
+     */
+    @Parameter(property = "solrHome", alias = "${solr.solr.home}",
+            defaultValue = "${project.build.directory}/solr")
+    protected File solrHome;
+
+    /**
+     * The entry point to Aether, i.e. the component doing all the work.
+     *
+     * @since 0.0.1
+     */
+    @Component
+    protected RepositorySystem repositorySystem;
+
+    /**
+     * The current repository/network configuration of Maven.
+     *
+     * @since 0.0.1
+     */
+    @Component
+    @Parameter(defaultValue = "${repositorySystemSession}")
+    protected RepositorySystemSession repositorySession;
+
+    /**
+     * The project's remote repositories to use for the resolution.
+     *
+     * @since 0.0.1
+     */
+    @Parameter(defaultValue = "${project.remoteProjectRepositories}",
+            readonly = true)
+    protected List<RemoteRepository> remoteRepositories;
+
+}
