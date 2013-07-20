3ace56db0c3a462513bce15f519c5f3ca3f99f63
Dmytro Chyzhykov (ffbit) <dmytro.chyzhykov@yandex.ru>
2013-07-20 22:55:48 +0300

    Every time configuration bootstrapping strategy test

--- src/test/java/com/ffbit/maven/solr/extract/NeverBootstrapMojoTest.java
+++ src/test/java/com/ffbit/maven/solr/extract/NeverBootstrapMojoTest.java
@@ -11,26 +11,4 @@ public class NeverBootstrapMojoTest extends AbstractSolrMojoExecutionTest {
         return new File(getBasedir(), "src/test/resources/poms/bootstrap/never/pom.xml");
     }
 
-    @Override
-    public void testStartWorkWithDefaultConfiguration() throws Exception {
-        try {
-            super.testStartWorkWithDefaultConfiguration();
-//            fail("It should never get here");
-        } catch (Exception e) {
-            System.out.println("Hello");
-            e.printStackTrace();
-            // all is OK
-        }
-    }
-
-    @Override
-    public void testRunWorkWithDefaultConfiguration() throws Exception {
-        try {
-            super.testRunWorkWithDefaultConfiguration();
-//            fail("It should never get here");
-        } catch (Exception e) {
-            // all is OK
-        }
-    }
-
 }
